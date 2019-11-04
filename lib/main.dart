c580f2b457f04ffa27d49239730d4848b2f6e846
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2019-11-04 14:49:04 +0200

    simple redux app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/services/authentication.dart';
-import 'package:test_project/pages/root_page.dart';
+import 'package:test_project/first/services/authentication.dart';
+import 'package:test_project/first/pages/root_page.dart';
+
+import 'package:test_project/redux/MyApp.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Flutter login demo',
-        debugShowCheckedModeBanner: false,
-        theme: new ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: new RootPage(auth: new Auth()));
-  }
-}
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+//        title: 'Flutter login demo',
+//        debugShowCheckedModeBanner: false,
+//        theme: new ThemeData(
+//          primarySwatch: Colors.blue,
+//        ),
+//        home: new RootPage(auth: new Auth()));
+//  }
+//}
